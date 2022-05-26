bcbbf0065a6fac8860c91fb7ac0112370aa32c01
nehusmaster <38113147+nehusmaster@users.noreply.github.com>
2022-05-26 16:54:12 -0300

    entrega final
    corrección que hice

---
+++ README.txt
@@ -0,0 +1,4 @@
+Alumno: Nehuén Alejandro Vazquez
+
+La transición que utilicé está en el
+index la imagen abajo del video
--- js/funciones.js
+++ js/funciones.js
@@ -1,2 +1,48 @@
+const nombre = document.getElementById("name");
+const correo = document.getElementById("email");
+const tema = document.getElementById("subject");
+const mensaje = document.getElementById("message");
+
+let btnForm = document.getElementById('btnForm');
+
+btnForm.addEventListener('click', function () {
+
+    let confirmacionnumero = 0;
+
+    let warnings = "";
+
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if (regexEmail.test(correo.value)) {
+        confirmacionnumero++;
+    } else {
+        alert('el email ingresado es incorrecto');
+    }
+
+    if (tema.value == '') {
+        alert('Por favor ingrese un tema');
+        confirmacionnumero = 0;
+    } else {
+        confirmacionnumero++;
+    }
+
+    if (mensaje.value == '') {
+        alert('Por favor ingrese un mensaje');
+        confirmacionnumero = 0;
+    } else {
+        confirmacionnumero++;
+    }
+
+    if(nombre.value == ''){
+        alert('Por favor ingrese un nombre');
+        confirmacionnumero = 0;
+    }else{
+        confirmacionnumero++;
+    }
+
+    if(confirmacionnumero == 4){
+        alert('El mensaje se envió');
+    }
 
 
+})
